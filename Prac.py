14c1bf7cf821dd028995645ee0c37ab9169eb0e5
Sam-2704 <128578020+Sam-2704@users.noreply.github.com>
2023-03-22 19:48:20 +0530

    Update Prac.py

--- Prac.py
+++ Prac.py
@@ -13,23 +13,12 @@ choice = st.sidebar.selectbox("Select an option", menu)
 
 # Define the content for each menu option
 if choice == "Home":
-    st.write("Welcome to the Life Tracking App! Choose an option from the sidebar to get started.")
-elif choice == "Sleep Tracker":
-      st.subheader("Sleep Tracking")
-    hours = st.number_input("Enter the number of hours of sleep", min_value=0, step=1)
-    time = get_time()
-    if st.button("Add sleep"):
-        st.write("f{time} - {hours} hours of sleep")
+  # Define home form here
+    pass
         
 elif choice == "Exercise Tracker":
-    st.subheader("Exercise Tracker")
-    exercise = st.text_input("Enter your exercise")
-    weight = st.number_input("Enter your weight (lbs)", min_value=0, step=1)
-    sets = st.number_input("Enter number of sets", min_value=0, step=1)
-    reps = st.number_input("Enter number of reps", min_value=0, step=1)
-    time = get_time()
-    if st.button("Add workout"):
-        st.write(f"{time} - {exercise}: {sets} sets of {reps} reps at {weight} lbs")
+     # Define exercise tracking form here
+    pass
 elif choice == "Water Tracker":
     # Define water tracking form here
     pass
